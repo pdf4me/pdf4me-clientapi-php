986a6516bf533ad15c2a08a663cf5d9486a92fe8
Varun Satish <varun.satish@ynoox.in>
2018-12-21 17:22:24 +0530

    minor changes

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <title>Ynoox PHP API Client</title>
+    <title>Pdf4me PHP API Client</title>
     <style>
         .zengreen {
-            color: #78A300;
+            color: red;
         }
         li{
           padding-bottom: 4px;
@@ -20,7 +20,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2 well">
-            <h1 class="zengreen">Ynoox</h1>
+            <h1 class="zenred">Pdf4me</h1>
             <h5>To help you get started, coding examples for authentication and basic usage are available in the `samples` folder.</h5>
 
             <div class="clearfix">&nbsp;</div>
